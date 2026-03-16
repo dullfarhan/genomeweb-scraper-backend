c1b115e96c4016daa8000bdca1f49ef886ef5047
dullfarhan <dullfarhan@gmail.com>
2026-03-16 16:58:11 +0500

    fix: comment out Accept-Encoding header in browser headers
    - Commented out the 'Accept-Encoding' header in the browser headers to prevent potential issues with request handling.

--- main.py
+++ main.py
@@ -202,7 +202,7 @@ _BROWSER_HEADERS = {
         "q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8"
     ),
     "Accept-Language": "en-US,en;q=0.9",
-    "Accept-Encoding": "gzip, deflate",
+    # "Accept-Encoding": "gzip, deflate",
     "Connection": "keep-alive",
 }
 
